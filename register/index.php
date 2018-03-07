d84839c5023936f127fdd047cbf74bc046ddd825
peterkariukimutuura <kariukipeter30@gmail.com>
2018-03-07 08:20:34 +0300

    files rewrite

--- register/index.php
+++ register/index.php
@@ -60,13 +60,13 @@ include 'assets/register.php';
 
 
 	<div class="row">
-		<form id="form" method="post" action="assets/register.php">
+		<form id="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" onsubmit="return sendform();">
 			<div>
 				<div>
 					<label for="UserName">Username</label>
 				</div>
 				
-				<input class="form-control" type="text" placeholder="username" name="username">
+				<input class="form-control" type="text" placeholder="username" id="username" name="username">
 				<small class="username" id="errormessage"></small>
 			</div><br>
 			<div>
@@ -74,7 +74,7 @@ include 'assets/register.php';
 					<label for="email">Email</label>
 				</div><br>
 				
-				<input class="form-control" type="email" placeholder="add email here" name="email">
+				<input class="form-control" type="email" placeholder="add email here" id="email" name="email">
 				<small class="email" id="errormessage"></small>
 			</div><br>
 			<div>
@@ -83,7 +83,7 @@ include 'assets/register.php';
 				</div>
 				
 				<!-- <input type="text" placeholder="add occupation here" name="occupation"> -->
-				<select class="form-control" name="occupation" id="occupation">
+				<select id="occupation" class="form-control" name="occupation">
 					<option value="">Choose One</option>
 					<option value="Prosecutors">Prosecutors</option>
 					<option value="Clerks">Clerks</option>
@@ -97,7 +97,7 @@ include 'assets/register.php';
 					<label for="password">Password</label>
 				</div><br>
 				
-				<input class="form-control" type="password" placeholder="add password here" name="password">
+				<input id="password" class="form-control" type="password" placeholder="add password here" name="password">
 				<small class="password" id="errormessage"></small>
 			</div><br>
 			<div>
@@ -105,10 +105,10 @@ include 'assets/register.php';
 					<label for="password">Confirm Password</label>
 				</div><br>
 				
-				<input class="form-control" type="password" name="confirmpassword" placeholder="confirm Password">
+				<input class="form-control" type="password" id="confirmpassword" name="confirmpassword" placeholder="confirm Password">
 				<small class="confirmpassword" id="errormessage"></small>
 			</div><br>
-			<button id="submit" type="button">Submit</button>
+			<button id="submit" type="submit">Submit</button>
 
 		</form><br><br><br><hr>
 		<footer>
@@ -120,70 +120,105 @@ include 'assets/register.php';
 <script type="text/javascript" src="../js/jquery.js"></script>
 <!-- form validation using javascript -->
 <script>
+
+$(document).ready(function(){
+
+
+	$('input').click(function(){
+		$(this).val("");
+
+		//hide the small tag error logs
+
+		var small_class=$(this).attr('name');
+		$('.'+small_class).html('');
+
+		//then set default styles to the input
+
+		$(this).css({
+			'border':'1px solid #58cec8',
+			'outline':'none'
+		});
+
+	});
+
+        $('#occupation').change(function(){
+        	//hide the error message if exists
+        	$('.occupation').html("");
+			$(this).css({
+				'border':'1px solid #58cec8',
+				'outline':'none'
+			});
+        });
+
+	 	$('#submit').click(function(){		
+			$('form').find('.form-control').each(function(){
+				if ($(this).val()=="") {
+					var small_class=$(this).attr('name');
+					$('.'+small_class).text('This Field is Required');
+					$(this).css({
+						'border':'1px solid red',
+					});
+				}
+			});
+		});
+
 	
+});
+
+	function sendform(){
+
+		var username =document.getElementById("username").value;
+		var email =document.getElementById("email").value;
+		var password =document.getElementById("password").value;
+		var occupation =document.getElementById("occupation").value;
+		var confirmpassword =document.getElementById("confirmpassword").value;
+		console.log(username);
+		console.log(email);
+		console.log(occupation);
+		console.log(password);
+		console.log(confirmpassword);
+		if (password!==confirmpassword) {
+			$('.confirmpassword').css({
+				'margin-left': '-270px',
+			});
+			$('.confirmpassword').html("Passwords You Have Entered Don't match");
+			$('#password').css({
+				'border':'1px solid red'
+			});
+			$('#confirmpassword').css({
+				'border':'1px solid red'
+			});
+			
+			return false;
+		}
+		if (username!==""&&email!==""&&password!==""&&occupation!=="") {
+			return true;
+		}
+
+
+		return false;
+	}
 	// $(document).ready(function(){
 
 	// 	var validation=false;
 	// 	//for the sake of user experience lets clear all inputs and hide error messages when the user is about
 	// 	// to add a value
 
-	// 	$('input').click(function(){
-	// 		$(this).val("");
 
-	// 		//hide the small tag error logs
-
-	// 		var small_class=$(this).attr('name');
-	// 		$('.'+small_class).html('');
-
-	// 		//then set default styles to the input
-
-	// 		$(this).css({
-	// 			'border':'1px solid #58cec8',
-	// 			'outline':'none'
-	// 		});
-
-	// 	});
 
 	// 	//fthis is checking if the select tag has value
 
- //        $('#occupation').change(function(){
- //        	//hide the error message if exists
- //        	$('.occupation').html("");
-	// 		$(this).css({
-	// 			'border':'1px solid #58cec8',
-	// 			'outline':'none'
-	// 		});
- //        })
 
 
 
 
-	// 	$('#submit').click(function(){
+
 
 			
 	// 		//we get all the all the inputs elements and loop through them
 	// 		//checks if all fields have value
 
-	// 		$('form').find('.form-control').each(function(){
-	// 			if ($(this).val()=="") {
-	// 				var small_class=$(this).attr('name');
-	// 				$('.'+small_class).text('This Field is Required');
-	// 				$(this).css({
-	// 					'border':'1px solid red',
-	// 				});
 
-	// 				validation=false;
-	// 			}else{
-
-	// 			validation=true;
-
-				 
-				
-
-
-	// 			}
-
-	// 		});
 
 	// 		if (validation==true) {
 	// 			document.getElementById("form").submit();
